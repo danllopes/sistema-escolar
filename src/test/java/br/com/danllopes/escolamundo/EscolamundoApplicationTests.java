21456ac4c4094c5df6f804536dea192ab405eb2d
“Daniel <danielanderson.lopes0@gmail.com>
2024-04-28 21:03:52 -0300

    Primeiro commit

---
+++ src/test/java/br/com/danllopes/escolamundo/EscolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.escolamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
